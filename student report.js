4004b9c1c99341bb5e49c1b8d544108f629667bd
RAVULAPALLI VENKATESWARLU <51966573+venky1469@users.noreply.github.com>
2019-08-29 20:07:58 +0530

    Add files via upload

---
+++ student report.js
@@ -0,0 +1,77 @@
+export const student details = [
+    {
+        "Date": 21-02-2019,
+        "city": "kurnool",
+        "district": "kurnool",
+        "college" :  "pullareddycollege"
+        "count":"5"
+        "SupplierID": 1,
+        "CategoryID": 1,
+        "QuantityPerUnit": "10 boxes x 20 bags",
+        "UnitPrice": 18,
+        "UnitsInStock": 39,
+        "UnitsOnOrder": 0,
+        "ReorderLevel": 10,
+        "Discontinued": false,
+        "Category": {
+
+        "Date": 21-02-2019,
+        "city": "kurnool",
+        "district": "kurnool",
+        "college" :  "pullareddycollege"
+        "count":"5"
+
+              },
+        "First person joing": new Date(2019,21,02)
+    },
+    {
+        "Date": 21-02-2019,
+        "city": "allagada",
+        "district": "kurnool",
+        "college" :  "govt degree college"
+        "count":"5"
+        "SupplierID": 1,
+        "CategoryID": 1,
+        "QuantityPerUnit": "10 boxes x 20 bags",
+        "UnitPrice": 18,
+        "UnitsInStock": 39,
+        "UnitsOnOrder": 0,
+        "ReorderLevel": 10,
+        "Discontinued": false,
+        "Category": {
+
+        "Date": 21-02-2019,
+        "city": "allagada",
+        "district": "kurnool",
+        "college" :  "Govtdegreecollege"
+        "count":"5"
+
+              },
+        "second person joing": new Date(2019,21,02)
+    },
+    {
+        "Date": 22-02-2019,
+        "city": "anatapur",
+        "district": "anatapur",
+        "college" :  "kotla engineering college"
+        "count":"3"
+        "SupplierID": 1,
+        "CategoryID": 1,
+        "QuantityPerUnit": "10 boxes x 20 bags",
+        "UnitPrice": 18,
+        "UnitsInStock": 39,
+        "UnitsOnOrder": 0,
+        "ReorderLevel": 10,
+        "Discontinued": false,
+        "Category": {
+
+        "Date": 22-02-2019,
+        "city": "anatapur",
+        "district": "anatapur",
+        "college" :  "kotla engineering college"
+        "count":"3"
+
+              },
+        "First person joing": new Date(2019,21,02)
+    },
+    
